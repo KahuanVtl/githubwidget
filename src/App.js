d8f2355850898314b9f219ed92d014b01d206b0c
kahuan <kahuanvitelli@gmail.com>
2024-08-27 23:49:16 -0300

    Adicionado CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Relogio from './Relogio'; // Importa o componente Relogio
 function App() {
   return (
     <div className="App">
-      <h1>Meu Aplicativo React</h1>
       <Relogio /> {/* Usa o componente Relogio */}
     </div>
   );
