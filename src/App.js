cb31bdccc686755c034204a67c1a3b6de805d065
kahuan <kahuanvitelli@gmail.com>
2024-08-27 23:45:47 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Relogio from './Relogio'; // Importa o componente Relogio
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Aplicativo React</h1>
+      <Relogio /> {/* Usa o componente Relogio */}
+    </div>
+  );
+}
+
+export default App;
